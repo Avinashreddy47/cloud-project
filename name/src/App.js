d3f993a53cf1c0f2aa78669c120acdc45ddff4dd
Avinash Reddy Sallagonda <40892223+Avinashreddy47@users.noreply.github.com>
2022-12-05 14:39:09 -0600

    Add files via upload

--- name/src/App.js
+++ name/src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 
 function App() {
   return <div> 
+    {/* <head>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+    </head> */}
     <Home />
+
   </div>;
   
 }
--- name/src/Home.js
+++ name/src/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useState } from "react";
+import toast,{Toaster} from 'react-hot-toast';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Home = () => {
   const [status, setStatus] = useState("Submit");
@@ -15,13 +17,37 @@ const Home = () => {
       message: message.value,
 
     };
-    let response = await fetch("http://localhost:4000/Home", {
+    console.log(details);
+    if(details.name==''){
+      toast.error('Please enter a firstname',{autoClose:2000}) 
+    }
+    if(details.last==''){
+      // console
+      toast.error('Please enter a last name',{autoClose:2000}) 
+    }
+    if(details.email==''){
+      toast.error('Please enter an email',{autoClose:2000}) 
+    }
+    if(details.query==''){
+      toast.error('Please enter a message',{autoClose:2000}) 
+    }
+    if(details.message==''){
+      toast.error('Please enter a company name',{autoClose:2000}) 
+    }
+    let response = await fetch("http://34.66.47.168/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
       },
       body: JSON.stringify(details),
     });
+    console.log(response);
+    if(response.message==="logged in sucessfully"){
+      toast.error('details entered successfully',{autoClose:2000}) 
+    }
+    else{
+      toast.error('Please enter valid details',{autoClose:2000}) 
+    }
     if(response!=null){
   			sessionStorage.setItem('token',response.data.token);
 				this.props.history.push('/');
@@ -33,12 +59,15 @@ const Home = () => {
   return (
    
       <>
+       {/* <head>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+    </head> */}
         <div>
           <h1 style={{ textAlign: "center" }}>Query Collector</h1>
         </div>
 
         <div className="container">
-
+        <Toaster/>
           <form onSubmit={handleSubmit}>
             <label htmlFor="fname">First Name</label>
             <input type="text" id="name" name="name" placeholder="Your name.." />
